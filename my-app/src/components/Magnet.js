6a1b07c8086e20212e58ecdf40645215ec23b54f
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2025-01-04 22:27:35 +1100

    Animation updaate magnet now pulsates

--- my-app/src/components/Magnet.js
+++ my-app/src/components/Magnet.js
@@ -1,10 +1,11 @@
 import React from "react";
 import magnetImage from "../Images/magnet_bg_removed.png";
 
-const Magnet = React.forwardRef((_, ref) => {
+const Magnet = React.forwardRef(({ onClick }, ref) => {
   return (
     <div
       ref={ref}
+      onClick={onClick} // Handle click event
       style={{
         position: "fixed",
         left: "30px", // Adjusted to center the larger image better
@@ -13,6 +14,15 @@ const Magnet = React.forwardRef((_, ref) => {
         width: "120px", // Increased size
         height: "120px", // Increased size
         zIndex: 5, // Ensure it appears above other elements
+        cursor: "pointer",
+        animation: "pulseHeat 3s infinite", // Slower pulsation with heat effect
+        transition: "transform 0.3s ease, filter 0.3s ease",
+      }}
+      onMouseEnter={(e) => {
+        e.target.style.transform = "scale(1.2) rotate(-5deg)"; // Scale and rotate on hover
+      }}
+      onMouseLeave={(e) => {
+        e.target.style.transform = "scale(1) rotate(0)"; // Reset transformation
       }}
     >
       <img
@@ -22,6 +32,7 @@ const Magnet = React.forwardRef((_, ref) => {
           width: "100%",
           height: "100%",
           objectFit: "contain", // Prevents cutting off parts of the image
+          filter: "brightness(1)", // Default brightness
         }}
       />
     </div>
